f831fe59b51a8b057d191ca2a933b80d03652e45
Beatriz <bsciamarelli@gmail.com>
2024-09-08 21:00:33 -0300

    Projetos feito no Coders2024

--- src/Mult.java
+++ src/Mult.java
@@ -1,19 +1,19 @@
-//import java.util.Scanner;
-//
-//public class Mult {
-//    public static void main(String[] args) {
-//
-//        Scanner scan = new Scanner(System.in);
-//        System.out.print("Digite o número para a tabuada: ");
-//        int num = scan.nextInt();
-//
-//        // Tabuada 5
-//
-//
-//        for (int i = 1; i <= 10; ++i) {
-//            int mult = num * i;
-//            System.out.println(num + "X" + i + " = " + mult);
-//        }
-//    }
-//}
-//
+import java.util.Scanner;
+
+public class Mult {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite o número para a tabuada: ");
+        int num = scan.nextInt();
+
+        // Tabuada 5
+
+
+        for (int i = 1; i <= 10; ++i) {
+            int mult = num * i;
+            System.out.println(num + "X" + i + " = " + mult);
+        }
+    }
+}
+
